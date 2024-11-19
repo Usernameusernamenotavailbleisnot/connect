0042ef3ac0b1dd7eb1728196d849c270c4e670e8
Usernameusernamenotavailbleisnot <rizkic0d3@gmail.com>
2024-11-19 09:25:08 +0700

    Update yescoingold.js

--- core/yescoingold.js
+++ core/yescoingold.js
@@ -6,7 +6,7 @@ const { Buffer } = require('buffer');
 const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const API = {
-    BASE_URL: "https://api-backend.yescoin.gold",
+    BASE_URL: "https://bi.yescoin.gold",
     MANIFEST: "https://www.yescoin.gold/tonconnect-manifest.json"
 };
 
@@ -18,13 +18,13 @@ class YesCoinService {
             'content-type': 'application/json',
             'origin': 'https://www.yescoin.gold',
             'referer': 'https://www.yescoin.gold/',
-            'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128", "Microsoft Edge WebView2";v="128"',
+            'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99", "Microsoft Edge WebView2";v="130"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0'
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0'
         };
         this.token = null;
         this.wallet = null;
